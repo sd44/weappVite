be5cca173ae99131e2818d7c3c08e59715a0426a
sd44 <sd44sd44@yeah.net>
2025-08-24 16:16:53 +0800

    feat(add html-eslint for fomrat wxml file): add html-eslint for format wxml

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import html from "@html-eslint/eslint-plugin"
+import eslintConfigPrettier from "eslint-config-prettier"
+
+export default [
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.wxml", "**/*.html"],
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/.vscode/**",
+      "**/idea/**",
+      "**/*.log",
+      "**/*.DS_Store",
+    ],
+    rules: {
+      ...html.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
+      "@html-eslint/indent": ["error", 2],
+      "@html-eslint/require-closing-tags": ["warn", { selfClosingCustomPatterns: ["-"] }],
+    },
+  },
+  eslintConfigPrettier,
+]
